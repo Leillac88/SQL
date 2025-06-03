ac5ee36bbf6170d4449ed68937ad3fb50a064918
Leilla Carvalho <148092822+Leillac88@users.noreply.github.com>
2025-06-03 08:43:56 -0300

    Update SELECT-names.sql

--- SELECT-names.sql
+++ SELECT-names.sql
@@ -1,45 +1,45 @@
-SELECT name FROM world
-  WHERE name LIKE 'Y%'
+1. SELECT name FROM world
+    WHERE name LIKE 'Y%'
 
-SELECT name FROM world
-  WHERE name LIKE '%y'
+2. SELECT name FROM world
+    WHERE name LIKE '%y'
 
-SELECT name FROM world
-  WHERE name LIKE '%x%'
+3. SELECT name FROM world
+    WHERE name LIKE '%x%'
 
-SELECT name FROM world
-  WHERE name LIKE '%land'
+4. SELECT name FROM world
+    WHERE name LIKE '%land'
 
-SELECT name FROM world
-  WHERE name LIKE 'C%ia'
+5. SELECT name FROM world
+    WHERE name LIKE 'C%ia'
 
-SELECT name FROM world
-  WHERE name LIKE '%oo%'
+6. SELECT name FROM world
+    WHERE name LIKE '%oo%'
 
-SELECT name FROM world
-  WHERE name LIKE '%a%a%a%'
+7. SELECT name FROM world
+    WHERE name LIKE '%a%a%a%'
 
-SELECT name FROM world
- WHERE name LIKE '_t%'
-ORDER BY name 
+8. SELECT name FROM world
+     WHERE name LIKE '_t%'
+    ORDER BY name 
 
-SELECT name FROM world
- WHERE name LIKE '%o__o%'
+9. SELECT name FROM world
+     WHERE name LIKE '%o__o%'
 
-SELECT name FROM world
- WHERE name LIKE '____'
+10. SELECT name FROM world
+     WHERE name LIKE '____'
 
-SELECT name
-  FROM world
- WHERE name LIKE capital
+11. SELECT name
+      FROM world
+     WHERE name LIKE capital
 
-SELECT name 
-  FROM world
- WHERE capital LIKE concat(name, ' City')
+12. SELECT name 
+      FROM world
+    WHERE capital LIKE concat(name, ' City')
 
-SELECT capital, name 
-  FROM world
- WHERE capital LIKE concat ('%' , name, '%')
+13. SELECT capital, name 
+      FROM world
+     WHERE capital LIKE concat ('%' , name, '%')
 
 14. ?
 
